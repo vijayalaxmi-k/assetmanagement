bbac52876496e29df04b655530e66d4b2cae261c
vijayalaxmi-k <57975240+vijayalaxmi-k@users.noreply.github.com>
2020-09-15 15:45:05 +0530

    Update db.connection.js

--- dbconnection/db.connection.js
+++ dbconnection/db.connection.js
@@ -1,10 +1,30 @@
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
+/*const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'dev_instadb',
   })
 
-  module.exports=pool;
+  module.exports=pool;*/
+/*import 'dbconnection\.env';
+
+
+
+export const HOST = process.env.HOST;
+export const USER = "root";
+export const PASSWORD = "";
+export const DB = "dev_instadb";*/
+
+var http = require("http");
+
+module.exports = {
+    HOST : "secure-myadmin-dev-instance.vedya.biz",
+    USER : "dev_instadb",
+    PASSWORD: "sdhgd9JDB",
+    DB :"dev_instadb",
+    PORT: 3306
+}; 
+
+
